baebb2384a09ad64d4fd95bdab77f489d0804174
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-10-29 08:41:54 +0100

    feat: add tasks module and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TasksController } from './tasks/tasks.controller';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
+  imports: [TasksModule],
+  controllers: [TasksController],
 })
 export class AppModule {}
