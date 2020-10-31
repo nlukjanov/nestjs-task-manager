33293c340f4a8677366c6bcf03927b1fe4461fdd
Nikita Lukjanov <nikita.lukjanov@nandos.co.uk>
2020-10-31 11:46:04 +0100

    chore: remove unused import

--- src/tasks/pipes/task-status-validation.pipe.ts
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -1,8 +1,4 @@
-import {
-  ArgumentMetadata,
-  BadRequestException,
-  PipeTransform,
-} from '@nestjs/common';
+import { BadRequestException, PipeTransform } from '@nestjs/common';
 import { TaskStatus } from '../task.model';
 
 export class TaskStatusValidationPipe implements PipeTransform {
