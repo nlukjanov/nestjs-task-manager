4da130be77410ebe4346af500ac6a9405c1d7df7
Nikita Lukjanov <nikita.lukjanov@nandos.co.uk>
2020-11-01 08:47:20 +0100

    feat: setup postgres and typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 import { TasksController } from './tasks/tasks.controller';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule],
   controllers: [TasksController],
 })
 export class AppModule {}
