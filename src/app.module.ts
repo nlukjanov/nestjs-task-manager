132d6a0661a493b55d68251070162257bf8d2a34
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-11-01 15:21:03 +0100

    feat: setup auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { TasksController } from './tasks/tasks.controller';
 import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule, AuthModule],
   controllers: [TasksController],
 })
 export class AppModule {}
