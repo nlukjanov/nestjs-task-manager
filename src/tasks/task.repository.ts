5339887704dc9444252c375304230de561caf43f
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-11-01 11:56:29 +0100

    feat: add filtering and search via query builder

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -7,7 +7,20 @@ import { Task } from './task.entity';
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
   async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    const { status, search } = filterDto;
     const query = this.createQueryBuilder('task');
+
+    if (status) {
+      query.andWhere('task.status = :status', { status });
+    }
+
+    if (search) {
+      query.andWhere(
+        'task.title LIKE :search OR task.description LIKE :search',
+        { search: `%${search}` },
+      );
+    }
+
     const tasks = await query.getMany();
     return tasks;
   }
